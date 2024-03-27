0c2284c29eb6ea02a37a35955a3b28f6b8048066
Nadja Kanunnikow <nadja.kanunnikow@gmx.de>
2024-03-27 09:58:43 +0100

    complete step42

--- script.js
+++ script.js
@@ -24,7 +24,8 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function goStore() {
-  console.log("Going to store.");
+  const button1 = document.querySelector("#button1");
+  button1.innerText = "Buy 10 health (10 gold)";
 }
 
 function goCave() {
