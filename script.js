0d1f811c767b741fe1f20f266e79966f32143807
Nadja Kanunnikow <nadja.kanunnikow@gmx.de>
2024-04-02 06:42:16 +0200

    complete step54

--- script.js
+++ script.js
@@ -18,15 +18,41 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
+const locations = [
+  {
+    name: "town square",
+    "button text": [],
+  },
+];
 // initialize buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
+function update(location) {}
+function goTown() {
+  button1.innerText = "Go to store";
+  button2.innerText = "Go to cave";
+  button3.innerText = "Fight dragon";
+  button1.onclick = goStore;
+  button2.onclick = goCave;
+  button3.onclick = fightDragon;
+  text.innerText =
+    'You are in the town square. You see a sign that says "Store".';
+}
 function goStore() {
   const button1 = document.querySelector("#button1");
   button1.innerText = "Buy 10 health (10 gold)";
+  button2.innerText = "Buy weapon (30 gold)";
+  button3.innerText = "Go to town square";
+  button1.onclick = buyHealth;
+  button2.onclick = buyWeapon;
+  button3.onclick = goTown;
+  text.innerText = "You enter the store.";
 }
+
+function buyHealth() {}
+function buyWeapon() {}
 
 function goCave() {
   console.log("Going to cave.");
